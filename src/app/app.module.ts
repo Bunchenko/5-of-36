007bc0fdc6bb4819f32eeebb4ade571164ca2855
Bunchenko <andrew.by.one@gmail.com>
2023-09-07 19:45:53 +0300

    setup base project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CardComponent } from './card/card.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
+import { TotalComponent } from './total/total.component';
+import { ButtonComponent } from './button/button.component';
+import { DrawingComponent } from './drawing/drawing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    CheckoutComponent,
+    CardComponent,
+    DropdownComponent,
+    TotalComponent,
+    ButtonComponent,
+    DrawingComponent
   ],
   imports: [
     BrowserModule,
