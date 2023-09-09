95a8e00d3f05409974aec372dc4644810b5af694
Bunchenko <andrew.by.one@gmail.com>
2023-09-09 18:17:02 +0300

    Revert "add method to count matches amount"
    This reverts commit 851ffad18ecda675de345bbcbb2e7db80a09826f.

--- src/services/balls.service.ts
+++ src/services/balls.service.ts
@@ -1,19 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Combination } from 'src/types/combination';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BallsService {
-  private _winCombination?: number[] = this.randomizeBalls(6);
-
-  constructor() {
-    console.log(this._winCombination);
-    const player = this.randomizeBalls(5);
-    console.log(player);
-
-    console.log(this.countMatchAmount(player));
-  }
+  constructor() {}
 
   public randomizeBalls(length: number): number[] {
     const allNumbers: number[] = Array.from({ length: 36 }, (_, i) => i + 1);
@@ -28,19 +19,5 @@ export class BallsService {
     return result;
   }
 
-  public countMatchAmount(playerBalls: number[]): Combination {
-    const bonusBall = this._winCombination?.pop();
-    let matchCount = 0;
-    let hasBonus = false;
-
-    for (let ball of playerBalls) {
-      if (this._winCombination?.includes(ball)) {
-        matchCount++;
-      } else if (!hasBonus && ball === bonusBall) {
-        hasBonus = true;
-      }
-    }
-
-    return { matchCount, hasBonus };
-  }
+  public countMatchAmount() {}
 }
--- src/types/combination.ts
+++
@@ -1,4 +0,0 @@
-export interface Combination {
-  matchCount: number;
-  hasBonus: boolean;
-}
