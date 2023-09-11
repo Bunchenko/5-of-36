dca6b2dd24c3005bc523d7e199832d229a757972
Bunchenko <andrew.by.one@gmail.com>
2023-09-11 20:17:33 +0300

    edit app and drawing templates

--- src/app/drawing/drawing.component.ts
+++ src/app/drawing/drawing.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { DrawingService } from 'src/services/drawing.service';
 
 @Component({
   selector: 'app-drawing',
   templateUrl: './drawing.component.html',
-  styleUrls: ['./drawing.component.css']
+  styleUrls: ['./drawing.component.css'],
 })
 export class DrawingComponent {
+  constructor(private _drawingService: DrawingService) {}
 
+  onClick() {
+    this._drawingService.complete$.next(true);
+  }
 }
