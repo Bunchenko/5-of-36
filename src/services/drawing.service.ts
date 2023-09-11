4d2cd3235439431ed9d8db434726c996ae500ac9
Bunchenko <andrew.by.one@gmail.com>
2023-09-11 12:50:01 +0300

    make timer observable multicast and move countdown to a servcie

--- src/services/drawing.service.ts
+++ src/services/drawing.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, interval, Subject } from 'rxjs';
-import { take, takeUntil, map } from 'rxjs/operators';
+import { take, takeUntil, map, tap, shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -8,12 +8,17 @@ import { take, takeUntil, map } from 'rxjs/operators';
 export class DrawingService {
   public timer$?: Observable<number>;
   public complete = new Subject<boolean>();
-  private _seconds = 10;
+  public time = 0;
+  private _seconds = 60;
 
   constructor() {
     this.timer$ = interval(1000).pipe(
+      shareReplay(1),
       takeUntil(this.complete),
       take(this._seconds + 1),
+      tap((v) => {
+        this.time = this._seconds - v;
+      }),
       map((v) => this._seconds - v)
     );
   }
