176b0c06bdb64e1543d014cc5a342c879a60e79c
Bunchenko <andrew.by.one@gmail.com>
2023-09-11 12:48:01 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameComponent } from './game/game.component';
+import { DrawingComponent } from './drawing/drawing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/game', pathMatch: 'full' },
+  { path: 'game', component: GameComponent },
+  { path: 'drawing', component: DrawingComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BallsService } from 'src/services/balls.service';
+import { DrawingService } from 'src/services/drawing.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = '5-of-36';
+
+  constructor(
+    protected drawingService: DrawingService,
+    private _ballsService: BallsService
+  ) {}
+
+  ngOnInit(): void {
+    this.drawingService.timer$?.subscribe({
+      next: (v) => {},
+      complete: () => {
+        this.drawingService.time = 0;
+        console.log('Complete!!!');
+      },
+    });
+  }
+
+  onClick() {
+    this.drawingService.complete.next(true);
+  }
 }
