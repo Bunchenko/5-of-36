12d3e07348717edce71cbd204928ee911de331ae
Bunchenko <andrew.by.one@gmail.com>
2023-09-14 09:27:40 +0300

    add logic to drawing page

--- src/app/drawing/drawing.component.ts
+++ src/app/drawing/drawing.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { BallsService } from 'src/services/balls.service';
 import { DrawingService } from 'src/services/drawing.service';
+import { PrizeService } from 'src/services/prize.service';
 
 @Component({
   selector: 'app-drawing',
@@ -7,9 +9,21 @@ import { DrawingService } from 'src/services/drawing.service';
   styleUrls: ['./drawing.component.css'],
 })
 export class DrawingComponent {
-  constructor(private _drawingService: DrawingService) {}
+  constructor(
+    private _drawingService: DrawingService,
+    protected _ballsService: BallsService,
+    protected _prizeService: PrizeService
+  ) {}
 
   onClick() {
     this._drawingService.complete$.next(true);
+  }
+
+  trackByCombinationFn(index: number, combination: number[]): number {
+    return index;
+  }
+
+  trackByBallFn(index: number, ball: number): number {
+    return ball;
   }
 }
