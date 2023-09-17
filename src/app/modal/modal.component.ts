ba97769920b1ed14c726d981a8cb57463738dff5
Bunchenko <andrew.by.one@gmail.com>
2023-09-17 21:41:38 +0300

    add animations to modal component

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,14 +1,31 @@
+import {
+  trigger,
+  style,
+  state,
+  transition,
+  animate,
+} from '@angular/animations';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
+  animations: [
+    trigger('open', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('200ms', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [animate('200ms', style({ opacity: 0 }))]),
+    ]),
+  ],
 })
 export class ModalComponent {
   @Input() title: string = '';
   @Input() isOpened: boolean = false;
-  @Output() closed = new EventEmitter();
+  @Output()
+  closed = new EventEmitter();
 
   protected _close() {
     this.closed.emit();
