893e934fb25ff64576fcc918253aba3d192bec1c
Bunchenko <andrew.by.one@gmail.com>
2023-09-20 12:17:18 +0300

    add animation for  winning combination appearance

--- src/app/drawing/drawing.component.ts
+++ src/app/drawing/drawing.component.ts
@@ -1,3 +1,11 @@
+import {
+  trigger,
+  transition,
+  query,
+  stagger,
+  animate,
+  style,
+} from '@angular/animations';
 import { Component } from '@angular/core';
 import { BallsService } from 'src/services/balls.service';
 import { DrawingService } from 'src/services/drawing.service';
@@ -7,6 +15,25 @@ import { PrizeService } from 'src/services/prize.service';
   selector: 'app-drawing',
   templateUrl: './drawing.component.html',
   styleUrls: ['./drawing.component.css'],
+  animations: [
+    trigger('combinationSpin', [
+      transition('* <=> *', [
+        query(
+          ':enter',
+          [
+            style({ opacity: 0 }),
+            stagger(500, [
+              animate(
+                '500ms ease-in-out',
+                style({ transform: 'rotate(720deg)', opacity: 1 })
+              ),
+            ]),
+          ],
+          { optional: true }
+        ),
+      ]),
+    ]),
+  ],
 })
 export class DrawingComponent {
   constructor(
