8e0f810ff0a85e48c38f68fd46bc9fa9730fb91b
Bunchenko <andrew.by.one@gmail.com>
2023-09-20 11:30:32 +0300

    add checkout component

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { BallsService } from 'src/services/balls.service';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent {
-
+  constructor(protected _ballsService: BallsService) {}
 }
