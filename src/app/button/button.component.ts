537e570d0288c58fc32b24fa50ba6f9face2cb54
Bunchenko <andrew.by.one@gmail.com>
2023-09-21 13:40:12 +0300

    set change detection onPush

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,9 +1,16 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ButtonComponent {
   @Input() customClass?: 'primary' | 'secondary' | 'danger';
